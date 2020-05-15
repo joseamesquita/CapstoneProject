16a7a7358b254410d28447138914c6361984084c
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-15 10:34:57 -0700

    update return statement to display student info

--- university-registrar-react/src/components/UniversityRegistrar.js
+++ university-registrar-react/src/components/UniversityRegistrar.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import ReusableForm from './ReusableForm';
 
-export default function UniversityRegistrar(props) {
+export default function Student(props) {
   const { student } = props;
   const [isEditing, setIsEditing] = useState(false);
   let button = isEditing ? null : <button onClick={() => setIsEditing(!isEditing)}>edit?</button>
 
-  const handleEditRegistrarFormSubmission = event => {
+  const handleEditStudentFormSubmission = event => {
     event.preventDefault();
     const propertiesToUpdate = {
       studentName: event.target.studentName.value,
@@ -15,7 +15,22 @@ export default function UniversityRegistrar(props) {
       currentTerm: event.target.currentTerm.value,
       currentYear: event.target.currentYear.value,
     }
-    props.onEditRegistrar(student.studentID, propertiesToUpdate);
+    props.onEditStudent(student.studentID, propertiesToUpdate);
     setIsEditing(!isEditing);
   }
+
+  return (
+    <React.Fragment>
+      <h1>{student.studentName}</h1>
+      <p>Date Enrolled: {student.dateEnrolled}</p>
+      <p>Student Major: {student.major}</p>
+      <p>Current Term: {student.currentTerm} Current Year: {student.currentYear}</p>
+      {button}
+      {isEditing ? <ReusableForm formSubmissionHandler={handleEditStudentFormSubmission}
+        buttonText="save changes" /> : null}
+      <br></br>
+      <hr></hr>
+      <button onClick={() => props.onDeleteStudent(student.studentID)}>Delete!</button>
+    </React.Fragment>
+  )
 }
