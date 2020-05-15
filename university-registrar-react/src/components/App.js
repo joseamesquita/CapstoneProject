2297445557b6eef13969c50a1e18e30c8f520bb1
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-14 21:50:25 -0700

    add first component

---
+++ university-registrar-react/src/components/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './../App.css';
+import UniversityRegistrar from './UniversityRegistrar';
+
+function App() {
+  return (
+    <React.Fragment>
+      <UniversityRegistrarControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
