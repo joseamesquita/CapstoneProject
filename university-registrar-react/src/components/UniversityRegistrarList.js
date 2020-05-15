b52522ce31d6f3b8d264881ffc5c6da0cdbb0f8d
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-15 15:38:57 -0700

    intialize fetch request to local host

--- university-registrar-react/src/components/UniversityRegistrarList.js
+++ university-registrar-react/src/components/UniversityRegistrarList.js
@@ -4,4 +4,18 @@ import Student from './UniversityRegistrar';
 function StudentList() {
   const [studentList, setStudentList] = useState([]);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    get();
+  }, [])
+
+  async function addStudent(student) {
+    await fetch(`http://localhost:5000/api/Students`, {
+      method: 'Post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(student)
+    })
+  }
 }
