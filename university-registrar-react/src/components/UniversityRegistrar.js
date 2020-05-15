45b860ddbfc115624ec7d0e5a90a683d38575c89
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-15 10:26:07 -0700

    modify form submission form to edit the properties of the registrar form

--- university-registrar-react/src/components/UniversityRegistrar.js
+++ university-registrar-react/src/components/UniversityRegistrar.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReusableForm from './ReusableForm';
 
 export default function UniversityRegistrar(props) {
-  const { registrar } = props;
+  const { student } = props;
+  const [isEditing, setIsEditing] = useState(false);
+  let button = isEditing ? null : <button onClick={() => setIsEditing(!isEditing)}>edit?</button>
+
+  const handleEditRegistrarFormSubmission = event => {
+    event.preventDefault();
+    const propertiesToUpdate = {
+      studentName: event.target.studentName.value,
+      dateEnrolled: event.target.dateEnrolled.value,
+      major: event.target.major.value,
+      currentTerm: event.target.currentTerm.value,
+      currentYear: event.target.currentYear.value,
+    }
+    props.onEditRegistrar(student.studentID, propertiesToUpdate);
+    setIsEditing(!isEditing);
+  }
 }
