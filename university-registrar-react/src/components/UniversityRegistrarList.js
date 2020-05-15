5ab5cd12a818ca81586302c97ee9a1189de891af
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-15 14:55:41 -0700

    create function for student list

---
+++ university-registrar-react/src/components/UniversityRegistrarList.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import Student from './UniversityRegistrar';
+
+function StudentList() {
+  const [studentList, setStudentList] = useState([]);
+  const [error, setError] = useState(null);
+}
