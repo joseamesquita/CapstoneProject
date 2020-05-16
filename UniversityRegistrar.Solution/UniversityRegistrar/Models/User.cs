8661e3a45242a5b7d9cb5a6732d9ac28a665020e
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-16 13:46:09 -0700

    initialized user info with data types

--- UniversityRegistrar.Solution/UniversityRegistrar/Models/User.cs
+++ UniversityRegistrar.Solution/UniversityRegistrar/Models/User.cs
@@ -3,6 +3,10 @@ namespace UniveristyRegistrar.Models
   public class User
   {
     public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
 
+    public string Password { get; set; }
   }
 }
