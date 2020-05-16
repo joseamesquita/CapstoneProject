2587b55c6f4e8f13e5cd7683a456d932388ff255
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-16 00:24:17 -0700

    initialize DbContext

---
+++ UniversityRegistrar.Solution/UniversityRegistrar/Models/UniversityRegistrarContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace UniveristyRegistrar.Models
+{
+  public class UniveristyRegistrarContext : DbContext
+  {
+    public UniveristyRegistrarContext(DbContextOptions<UniveristyRegistrarContext> options)
+      : base(options)
+    {
+
+    }
+
+    public DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Student>()
+      .HasData(
+        new Student { StudentId = 1, StudentName = "Jose", DateEnrolled = "September 27, 2016", Major = "Mathematics & Computer Science", CurrentTerm = "Fall", CurrentYear = 2016 }
+      );
+    }
+  }
+}
