dfa40a6b1544fb438291794b52fadd321f6dd744
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-18 12:16:29 -0700

    add courseId, courseName

---
+++ UniversityRegistrar.Solution/UniversityRegistrar/Models/Course.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace UniveristyRegistrar.Models
+{
+  public class Course
+  {
+    public Course()
+    {
+      this.Students = new HashSet<Student>();
+    }
+
+    public int CourseId { get; set; }
+    public string CourseName { get; set; }
+    public virtual ICollection<Student> Students { get; set; }
+  }
+}
