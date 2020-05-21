d186d89abca2414e51f628c3a9bcab242b940add
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-21 02:40:59 -0700

    refactor layout and design of the site

--- UniversityRegistrar.Solution/UniversityRegistrar/Models/Student.cs
+++ UniversityRegistrar.Solution/UniversityRegistrar/Models/Student.cs
@@ -1,20 +1,28 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace UniversityRegistrar.Models
 {
   public class Student
   {
 
+    [Display(Name = "Student ID")]
     public int StudentId { get; set; }
 
+    // [Required]
+    [Display(Name = "Student Name")]
     public string StudentName { get; set; }
 
+    [Display(Name = "Date Enrolled")]
     public string DateEnrolled { get; set; }
 
+    [Display(Name = "Programming Language")]
     public string Major { get; set; }
 
+    [Display(Name = "Current Term")]
     public string CurrentTerm { get; set; }
 
+    [Display(Name = "Current Year")]
     public string CurrentYear { get; set; }
 
     public int CourseId { get; set; }
